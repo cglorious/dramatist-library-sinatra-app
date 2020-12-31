110c0a94182323895e351ce282dc051d49c35518
Catherine Gloria <catherinegloria@gmail.com>
2020-12-31 14:00:15 -0800

    Initial commit

---
+++ Notes.rb
@@ -0,0 +1,49 @@
+# Project set-up
+# [x] 1. Review Project Requirements
+#         - ORM ActiveRecord
+#           - schema
+#           - tux - access objects through ActiveRecord
+#         - Rack
+#           - Internet
+#           - Request/Response Flow
+#           - Routes
+#         - MVC and Forms
+#           - Model - database
+#           - View - frontend
+#           - Controller - intermediary
+#         - CRUD
+#           - rendering vs. redirecting
+#         - User Authentication
+#           - session
+#           - has_secure_password
+#               - .authenticate
+#           - bcrypt gem
+# [x] 2. Ideate
+#         - Dramatist library
+#         - Includes playwrights, plays, and characters
+# [x] 3. Wireframes, User Stories
+#         - models, attributes, associations
+#         - As a user, I can:
+#           Search for plays and characters
+#           Add play to virtual library
+#         - User should be able to:
+#             - Input play and character details for a new play
+#             - Locate list of plays or playwrights
+#             - Edit details of playwrights, plays, and characters
+#         - What does your app do?
+#           App allows users to create, edit, and view plays
+# [x] 4. MVP vs. stretch goals
+#         - MVP
+#             - Users can create, edit, and view plays
+#         - Stretch goals
+#             - Use seed data from Dramatists Play Service, Inc.
+#             - Incorporate pictures of plays in HTML
+# [x] 5. Corneal Gem
+# [x] 6. GitHub Repo
+# [ ] 7. Build models
+#         - Migrations
+#         - Model classes
+#         - Associations
+# [ ] 8. Test models and associations in console
+#         - Create seed data
+#         - Adjust migrations (as needed)
